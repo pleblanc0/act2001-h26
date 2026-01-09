7dcae74c8164c7825bac1ba03291ab93368769ad
Philippe Leblanc <phil.leblanc360@gmail.com>
2026-01-09 13:24:46 -0500

    add bases.R

--- ressources/bases.R
+++ ressources/bases.R
@@ -1,7 +1,6 @@
 ################################################################################
 #### Introduction à l'actuariat II - Hiver 2026 ################################
 #### École d'actuariat - Université Laval ######################################
-#### Auteur: Philippe Leblanc ##################################################
 ################################################################################
 
 ################################################################################
@@ -32,31 +31,51 @@ EspLim(5)
 EspTr <- function(d) sum(x * fx * I(x > d))
 EspTr(5)
 
-# Fonction stop-loss
-SL <- function(d) sum(pmax(x - d, 0) * fx)
-SL(5)
 
-# Fonction génératrice des moments
-FGM <- function(t) sum(exp(t * x) * fx)
-FGM(0.1)
+################################################################################
+### Partie II - Lois connues #################################################
+################################################################################
 
-# Fonction génératrice des probabilités
-FGP <- function(t) sum(t^x * fx)
-FGP(0.1)
+### Loi Gamma ##################################################################
 
-# Value at Risk
-VaR <- function(u) x[min(which(Fx >= u))]
-VaR(0.95)
+al <- 2
+be <- 0.01
 
-# Tail Value at Risk
-TVaR <- function(u) SL(VaR(u))/(1 - u) + VaR(u)
-TVaR(0.95)
+# Fonction de répartition
+pgamma(c(0, 50, 100), al, be)
 
-# Left Tail Value at Risk
-LTVaR <- function(u) (EspX - (1 - u) * TVaR(u))/u
-LTVaR(0.95)
+# Fonction de densité
+dgamma(c(0, 50, 100), al, be)
 
-# Range Value at Risk
-RVaR <- function(u1, u2) ((1 - u1) * TVaR(u1) - (1 - u2) * TVaR(u2))/(u2 - u1)
-RVaR(0.6, 0.95)
+# Fonction quantile
+qgamma(c(0, 0.5, 0.9), al, be)
 
+
+### Loi Poisson ################################################################
+
+lam <- 3
+
+# Fonction de répartition
+ppois(c(0, 5, 10), lam)
+
+# Fonction de densité
+dpois(c(0, 5, 10), lam)
+
+# Fonction quantile
+qpois(c(0, 0.5, 0.9), lam)
+
+
+################################################################################
+### Partie III - Optimisation numérique ########################################
+################################################################################
+
+### Optimize ###################################################################
+
+f <- function (x, a) (x - a)^2
+(xmin <- optimize(f, c(0, 1), a = 1/3))
+
+
+### Uniroot ####################################################################
+
+f <- function (x, a) x - a
+(xmin <- uniroot(f, c(0, 1), a = 1/3))
